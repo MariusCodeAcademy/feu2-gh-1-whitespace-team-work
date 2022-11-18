d67bac2d98884bab51d20fcf215d3de36e66256d
Marius <marius@example.com>
2022-11-18 15:40:52 +0200

    dynamic add hero

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,14 @@
-"use strict";
-console.log("main.js");
+'use strict';
+console.log('main.js');
 
 // kreipiames i html div elementus
-const headerEl = document.getElementById("header");
-const projectManagment = document.getElementById("project-management");
-const workTogether = document.getElementById("work-togethert");
+const headerEl = document.getElementById('header');
+const projectManagment = document.getElementById('project-management');
+const workTogether = document.getElementById('work-togethert');
+const heroEl = document.getElementById('hero');
 
 // daugkartine funkcija
-async function loadHeader(fromWhere, toWhere) {
+async function loadComponent(fromWhere, toWhere) {
   try {
     const resp = await fetch(fromWhere);
     const html = await resp.text();
@@ -18,10 +19,13 @@ async function loadHeader(fromWhere, toWhere) {
 }
 
 // Header
-loadHeader("components/header.html", headerEl);
+loadComponent('components/header.html', headerEl);
+
+// hero
+loadComponent('components/hero.html', heroEl);
 
 // ProjectManagment
-loadHeader("components/projectManagment.html", projectManagment);
+loadComponent('components/projectManagment.html', projectManagment);
 
 // Work together
-loadHeader("components/workTogether.html", workTogether);
+loadComponent('components/workTogether.html', workTogether);
